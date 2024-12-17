85a6056c8ac969c93f11d489a73de8bdf5556b1e
Kristians P <kperovs15@gmail.com>
2024-12-17 21:35:40 +0000

    Almost finished rythm minigame

---
+++ Assets/Scripts/Lane.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Melanchall.DryWetMidi.Interaction;
+using System;
+public class Lane : MonoBehaviour
+{
+    public Melanchall.DryWetMidi.MusicTheory.NoteName noteRestriction;
+    public KeyCode input;
+    public GameObject notePrefab;
+    List<Note> notes = new List<Note> ();
+    public List<double> timeStamps = new List<double>();
+
+    int spawnIndex = 0;
+    int inputIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void SetTimeStamps(Melanchall.DryWetMidi.Interaction.Note[] array)
+    {
+        foreach (var note in array) 
+        { 
+            if(note.NoteName == noteRestriction)
+            {
+                var metricTimeSpan = TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, SongManager.midiFile.GetTempoMap());
+                timeStamps.Add((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds + (double)metricTimeSpan.Milliseconds / 1000f);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnIndex < timeStamps.Count)
+        { 
+            if(SongManager.GetAudioSourceTime() >= timeStamps[spawnIndex] - SongManager.instance.noteTime)
+            {
+                var note = Instantiate(notePrefab, transform);
+                notes.Add(note.GetComponent<Note>());
+                note.GetComponent<Note>().assignedTime = (float)timeStamps[spawnIndex];
+                spawnIndex++;
+            }
+        }
+        if (inputIndex < timeStamps.Count)
+        {
+            double timeStamp = timeStamps[inputIndex];
+            double marginOfError = SongManager.instance.marginOfError;
+            double audioTime = SongManager.GetAudioSourceTime() - (SongManager.instance.inputDelayInMilliseconds / 1000.0);
+            if (Input.GetKeyDown(input))
+            {
+                if (Math.Abs(audioTime - timeStamp) < marginOfError)
+                {
+                    Hit();
+                    print($"Hit on{inputIndex} note ");
+                    Destroy(notes[inputIndex].gameObject);
+                    inputIndex++;
+                }
+                else
+                {
+                    print($"Hit innacurate on{inputIndex} note ");
+                }
+            }
+            if(timeStamp + marginOfError <= audioTime)
+            {
+                Miss();
+                print($"Hit Missed on{inputIndex} note ");
+                inputIndex++;
+            }
+        }
+    }
+    private void Hit() 
+    {
+        
+    }
+    private void Miss()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public AudioSource hitSFX;
+    public AudioSource missSFX;
+    public TMPro.TextMeshPro scoreText;
+    static int comboScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        comboScore = 0;
+    }
+    public static void Hit()
+    {
+        comboScore += 1;
+        instance.hitSFX.Play();
+    }
+    public static void Miss()
+    {
+        comboScore = 0;
+        instance.missSFX.Play();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        scoreText.text = comboScore.ToString();
+    }
+}
